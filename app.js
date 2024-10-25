98946f58a71d0d6f53d9b4700aa38aa34766a31b
duyguerogluu <erogluduyguduygu@gmail.com>
2024-10-25 23:05:34 +0300

    first commit

---
+++ app.js
@@ -0,0 +1,30 @@
+const express = require('express');
+const { faker } = require('@faker-js/faker');
+const app = express();
+const PORT = 3000;
+
+
+
+const generateFakeUsers = (num) =>{
+   const users = [];
+   for(let i=0; i<num; i++){
+    users.push({
+        id: i+1,
+        name: faker.name.firstName(),
+        surname: faker.name.lastName(),
+        image: faker.image.avatar(),
+        isActive: faker.datatype.boolean(),
+        Address:faker.address.streetAddress(),
+    })
+   }
+   return users;
+};
+
+app.get('/users',(req, res )=>{
+    const users = generateFakeUsers(5);
+    res.json(users);
+});
+
+app.get('/', (req, res) => {res.send('ok')});
+
+app.listen(PORT, ()=>{console.log(`Server is running on http://localhost:${PORT}`);});
